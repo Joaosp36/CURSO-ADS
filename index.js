3ecc37cce19759ba1d93dbdda3d457576ce75efa
joaovitor-xp <joaovitorsimaosilva1@gmail.com>
2026-03-10 20:20:49 -0300

    remover arquivo

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-const express = require("express")
 
-const app = express()
-
-app.use("/", function (req, res){
-    res.send("<h1>Funcionou</hi>")
-})
-
-app.listen(3000, () => {
-    console.log('Servidor rodando em http://localhost:3000')
-})  
